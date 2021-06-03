a866ad101693ce77d5fb4ebc3214073ff583f351
mas4erlomas4er <i9997117@gmail.com>
2021-06-03 11:31:55 +0300

    initrial commit

---
+++ script.js
@@ -0,0 +1,77 @@
+$( document ).ready( () => {
+		$( "html" ).niceScroll( {
+			cursorborder : "1px solid rgba(153,156,160, 0.5)",
+			cursorcolor : "rgb(174,186,207)",
+			cursoropacitymin : 0.4,
+			zindex : 1000
+		} );
+
+		jQuery.ajaxSetup( { async : false } );
+
+		const disablePreloader = ( delay ) => {
+			setTimeout( () => $( '.preloader' ).addClass( 'done' ), delay );
+		};
+		disablePreloader( 1500 );
+
+		$( window ).on( "scroll", () => {
+			let scrollDistance = $( window ).scrollTop();
+			if ( scrollDistance )
+				$( ".header" ).addClass( "fixed" );
+			else
+				$( ".header" ).removeClass( "fixed" );
+		} );
+
+		class PortfolioItem {
+			constructor ( id, title, description, image, url ) {
+				this.id = id;
+				this.title = title;
+				this.description = description;
+				this.image = image;
+				this.url = url;
+			}
+
+			getHTML () {
+				return $( `
+				<div class="card">
+					<div class="card-image" style="background-image: url(${ this.image })"></div>
+					<a href="${ this.url }" class="card-link" target="_blank">
+						<span class="card-visit">Visit</span>
+						<div class="card-btn"></div>
+					</a>
+					<div class="card-content">
+					<p class="card-title">${ this.title }</p>
+					<p class="card-description">${ this.description }</p>
+					</div>
+				</div>
+			` );
+			}
+		}
+
+		const parseData = response => {
+			return response.map( ( el, index ) => {
+				return new PortfolioItem(
+					index,
+					el[ "gsx$title" ][ "$t" ],
+					el[ "gsx$description" ][ "$t" ],
+					el[ "gsx$image" ][ "$t" ],
+					el[ "gsx$url" ][ "$t" ]
+				);
+			} );
+		};
+
+		const getSheet = id => {
+			let dataSheetUrl = `
+			https://spreadsheets.google.com/feeds/list/${ id }/od6/public/values?alt=json`;
+
+			$.getJSON( dataSheetUrl, response => {
+				let data = parseData( response[ "feed" ][ "entry" ] );
+				data.forEach( el => $( ".content" ).append( el.getHTML() ) );
+			} );
+		}
+
+		getSheet(
+			"1ptPkvzQbvcreLEGbPwPT5ieS384DbUSiaxDtu8ZOrl4"
+		)
+		;
+	}
+);
