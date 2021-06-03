a0025b1339cb70d43cb660293c9022c694937afa
Mas4erLomas4er <i9997117@gmail.com>
2021-06-03 15:40:16 +0300

    Refactor and optimize

--- script.js
+++ script.js
@@ -1,3 +1,57 @@
+$.ajaxSetup( { async : false } );
+
+const disablePreloader = ( delay ) => {
+	setTimeout( () => $( ".preloader" ).addClass( 'done' ), delay );
+};
+
+class PortfolioItem {
+	constructor ( id, title, description, image, url ) {
+		this.id = id;
+		this.title = title;
+		this.description = description;
+		this.image = image;
+		this.url = url;
+	}
+
+	render () {
+		return $( `
+			<div class="card">
+				<div class="card-image" style="background-image: url(${ this.image })"></div>
+				<a href="${ this.url }" class="card-link" target="_blank">
+					<span class="card-visit">Visit</span>
+					<div class="card-btn"></div>
+				</a>
+				<div class="card-content">
+				<p class="card-title">${ this.title }</p>
+				<p class="card-description">${ this.description }</p>
+				</div>
+			</div>
+		` );
+	}
+}
+
+const parseData = response => {
+	return response.map( ( el, index ) => {
+		return new PortfolioItem(
+			index,
+			el[ "gsx$title" ][ "$t" ],
+			el[ "gsx$description" ][ "$t" ],
+			el[ "gsx$image" ][ "$t" ],
+			el[ "gsx$url" ][ "$t" ]
+		);
+	} );
+};
+
+const getSheet = id => {
+	let dataSheetUrl = `https://spreadsheets.google.com/feeds/list/${ id }/od6/public/values?alt=json`;
+
+	$.getJSON( dataSheetUrl, response => {
+		let data = parseData( response[ "feed" ][ "entry" ] );
+		data.forEach( el => $( ".content" ).append( el.render() ) );
+		disablePreloader( 1000 );
+	} );
+}
+
 $( document ).ready( () => {
 		$( "html" ).niceScroll( {
 			cursorborder : "1px solid rgba(153,156,160, 0.5)",
@@ -6,72 +60,14 @@ $( document ).ready( () => {
 			zindex : 1000
 		} );
 
-		jQuery.ajaxSetup( { async : false } );
-
-		const disablePreloader = ( delay ) => {
-			setTimeout( () => $( '.preloader' ).addClass( 'done' ), delay );
-		};
-		disablePreloader( 1500 );
-
 		$( window ).on( "scroll", () => {
-			let scrollDistance = $( window ).scrollTop();
-			if ( scrollDistance )
+			if ( $( window ).scrollTop() )
 				$( ".header" ).addClass( "fixed" );
 			else
 				$( ".header" ).removeClass( "fixed" );
+
 		} );
 
-		class PortfolioItem {
-			constructor ( id, title, description, image, url ) {
-				this.id = id;
-				this.title = title;
-				this.description = description;
-				this.image = image;
-				this.url = url;
-			}
-
-			getHTML () {
-				return $( `
-				<div class="card">
-					<div class="card-image" style="background-image: url(${ this.image })"></div>
-					<a href="${ this.url }" class="card-link" target="_blank">
-						<span class="card-visit">Visit</span>
-						<div class="card-btn"></div>
-					</a>
-					<div class="card-content">
-					<p class="card-title">${ this.title }</p>
-					<p class="card-description">${ this.description }</p>
-					</div>
-				</div>
-			` );
-			}
-		}
-
-		const parseData = response => {
-			return response.map( ( el, index ) => {
-				return new PortfolioItem(
-					index,
-					el[ "gsx$title" ][ "$t" ],
-					el[ "gsx$description" ][ "$t" ],
-					el[ "gsx$image" ][ "$t" ],
-					el[ "gsx$url" ][ "$t" ]
-				);
-			} );
-		};
-
-		const getSheet = id => {
-			let dataSheetUrl = `
-			https://spreadsheets.google.com/feeds/list/${ id }/od6/public/values?alt=json`;
-
-			$.getJSON( dataSheetUrl, response => {
-				let data = parseData( response[ "feed" ][ "entry" ] );
-				data.forEach( el => $( ".content" ).append( el.getHTML() ) );
-			} );
-		}
-
-		getSheet(
-			"1ptPkvzQbvcreLEGbPwPT5ieS384DbUSiaxDtu8ZOrl4"
-		)
-		;
+		getSheet( "1ptPkvzQbvcreLEGbPwPT5ieS384DbUSiaxDtu8ZOrl4" );
 	}
 );
